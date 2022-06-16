33fd9be36210b3d8f432455f5a5f63e0bee76bfe
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:48:29 -0300

    Add files via upload

---
+++ 80-IngresoNombreYCiudad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*Pedir por consola un nombre de persona y el nombre de una ciudad (no hace falta que sean reales o comprobarlos)
+ * mostrar por pantalla, 
+ * el siguiente mensaje “Hola ” <nombre> ” bienvenido a ” <ciudad>
+ */
+
+namespace _80_IngresoNombreYCiudad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un nombre de persona y luego uno de ciudad:");
+
+            string n = Convert.ToString(Console.ReadLine());
+
+            string c = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Hola " + n + " " + "bienvenido a " + c);
+        }
+    }
+}
